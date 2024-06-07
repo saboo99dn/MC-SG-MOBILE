9c1d883861b3cc23fab23989e4afec59cfa193d0
saboo99dn <166354607+saboo99dn@users.noreply.github.com>
2024-06-07 12:26:17 +0700

    Update build_dev.sh

--- build_dev.sh
+++ build_dev.sh
@@ -1,6 +1,6 @@
 echo 'tesst ne'
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install fastlane
 
