ce683a980c1c57694780b3621d907c469fc76fbb
saboo99dn <166354607+saboo99dn@users.noreply.github.com>
2024-06-07 13:16:56 +0700

    Update build_dev.sh

--- build_dev.sh
+++ build_dev.sh
@@ -1,5 +1,5 @@
 echo 'tesst ne'
 
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'done ne'
