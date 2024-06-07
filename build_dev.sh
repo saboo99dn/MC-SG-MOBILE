a36d1b56124316b08f5a52332ba7dbb9744fa49c
saboo99dn <166354607+saboo99dn@users.noreply.github.com>
2024-06-07 13:19:15 +0700

    Update build_dev.sh

--- build_dev.sh
+++ build_dev.sh
@@ -1,6 +1,8 @@
 echo 'tesst ne'
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/jenkins/.zprofile
+source /Users/jenkins/.zprofile
 
 brew install fastlane
 
